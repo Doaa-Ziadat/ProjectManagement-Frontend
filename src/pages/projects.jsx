7ed56f8152b622cb6738654ab8791429ed877ee2
Doaa ziadat <doaaziadat@gmail.com>
2022-02-20 11:40:24 +0200

    ..

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -10,31 +10,4 @@ const Projects = () => {
     fetch("http://localhost:4000/projects")
       .then((res) => res.json())
       .then((data) => {
-        setProjects(data);
-        //   data.map((pr) =>
-        //    fetch(`http://localhost:4000/email/${pr.userid}`));
-        // })
-        // .catch((err) => {
-        //   console.log(err);
-      });
-  }, []);
-
-  return (
-    <div>
-      Projects :
-      <ul>
-        {projects.map((pr) => (
-          <li key={pr.id}>
-            <div> Project Name :{pr.name}</div>
-            <div> Timeline : {pr.timeline.slice(0, 10)}</div>
-            <div> Priority:{pr.priority}</div>
-            <div> Created By :{pr.userid}</div>
-            <div>Created At:{pr.createdat.slice(0, 10)}</div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default Projects;
+        set

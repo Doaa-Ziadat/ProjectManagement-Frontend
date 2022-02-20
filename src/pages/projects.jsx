a1a31c3e543f97019e866c1d9320b66bedb8d537
Doaa-Ziadat <doaaziadat@gmail.com>
2022-02-20 12:16:20 +0200

    mm

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const Projects = () => {
+  console.log("ghhgg");
   const [projects, setProjects] = useState([]);
   const [usersId, setUsersId] = useState([]);
   const [users, setUses] = useState([]);
@@ -10,4 +11,32 @@ const Projects = () => {
     fetch("http://localhost:4000/projects")
       .then((res) => res.json())
       .then((data) => {
-        set
+        setProjects(data);
+        //   data.map((pr) =>
+        //    fetch(`http://localhost:4000/email/${pr.userid}`));
+        // })
+        // .catch((err) => {
+        //   console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      Projects :
+      <ul>
+        {projects.map((pr) => (
+          <li key={pr.id}>
+            <div> Project Name :{pr.name}</div>
+            <div> Timeline : {pr.timeline.slice(0, 10)}</div>
+            <div> Priority:{pr.priority}</div>
+            <div> Created By :{pr.userid}</div>
+            <div>Created At:{pr.createdat.slice(0, 10)}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+// mar7baaaaaaa
+
+export default Projects;
